ee2030a1901e2586d72316f3c1519d8b4b223965
Cayiiiu <cayiiiu@hotmail.com>
2026-03-07 11:44:07 -0600

    Correccion inventario: suma y resta de cantidad funcionando

--- src/app/inventario/inventario.component.ts
+++ src/app/inventario/inventario.component.ts
@@ -1,11 +1,72 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-inventario',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './inventario.component.html',
   styleUrl: './inventario.component.css'
 })
-export class InventarioComponent {
+export class InventarioComponent implements OnInit {
+
+  productos:any[] = [];
+
+  constructor(private router: Router, private http: HttpClient){}
+
+  cerrarSesion(){
+    localStorage.removeItem("login");
+    this.router.navigate(['']);
+  }
+
+  ngOnInit(){
+
+    if(localStorage.getItem("login") !== "true"){
+      this.router.navigate(['']);
+    }
+
+    this.cargarProductos();
+
+  }
+
+  cargarProductos(){
+
+    this.http.get<any>('http://localhost/inventario_api/obtener_productos.php')
+    .subscribe(res => {
+
+      console.log(res);
+
+      this.productos = res.map((p:any) => ({
+        ...p,
+        cantidad: Number(p.cantidad)
+      }));
+
+    });
+
+  }
+
+  cambiarCantidad(producto:any, cambio:number){
+
+    let nuevaCantidad = producto.cantidad + cambio;
+
+    if(nuevaCantidad < 0){
+      nuevaCantidad = 0;
+    }
+
+    this.http.post<any>('http://localhost/inventario_api/actualizar_cantidad.php',{
+      id: producto.id,
+      cantidad: nuevaCantidad
+    }).subscribe(res => {
+
+      if(res.success){
+        producto.cantidad = nuevaCantidad;
+      }
+
+    });
+
+  }
 
 }
